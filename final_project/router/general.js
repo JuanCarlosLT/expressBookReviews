91aa63907b2d4d6834cdc62a43a9183b17721769
JuanCarlosLT <juan.lopezt@motivus.com>
2026-04-16 23:43:42 -0400

    Tarea10 Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,8 +20,19 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify(books,null,4));
+public_users.get('/',async function (req, res) {
+    try {
+        const getBooks = () => {
+            return new Promise((resolve, reject) => {
+                resolve(books);
+            });
+        };
+
+        const response = await getBooks();
+        return res.status(200).send(JSON.stringify(response, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Error al obtener los libros" });
+    }
 });
 
 // Get book details based on ISBN
